7e542e252a3c6727adde7a14bef74e0909fc4905
yano <yano@example.com>
2022-12-23 13:38:54 +0900

    fin

--- app/Http/Controllers/S3ClientController.php
+++ app/Http/Controllers/S3ClientController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\GetPresignedUrlRequest;
 use Aws\S3\S3Client;
-use Aws\S3\Transfer;
-use Aws\Exception\AwsException;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Str;
 
 class S3ClientController extends Controller
@@ -17,29 +14,25 @@ class S3ClientController extends Controller
     */
     public function getPresignedUrl(GetPresignedUrlRequest $request)
     {
+        // S3クライアント作成
         $s3Client = new S3Client([
             'region' => 'ap-northeast-1',
             'version' => 'latest',
         ]);
-
+        // GETリクエストのfailenameパラメーターを加工
         $filename = $this->makeUniqueFilename(
             pathinfo($request->filename, PATHINFO_EXTENSION)
         );
-
+        // コマンド作成（何のアクション、どのバケットに、S3オブジェクト保存用のキー名を添えて）
         $cmd = $s3Client->getCommand('PutObject', [
             'Bucket' => config('filesystems.disks.s3.bucket'),
             'Key' => $filename
         ]);
-
+        // 署名済みURLの取得（コマンドと有効期限を添えて）
         $presignedRequest = $s3Client->createPresignedRequest(
             $cmd,
             '+5 minutes'
         );
-
-        // return view('welcome',[
-        //     'filename' => $filename,
-        //     'pre_signed_url' => (string) $presignedRequest->getUri()
-        // ]);
         return response()->json(['filename' => $filename, 'pre_signed_url' => (string) $presignedRequest->getUri()]);
     }
 
@@ -47,6 +40,8 @@ class S3ClientController extends Controller
     * @param  string $extension
     * @return string
     */
+
+    // 一意のファイル名に加工するメソッド
     public function makeUniqueFilename($extenstion)
     {
         return (string) Str::uuid() . '.' . $extenstion;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,23 @@
 <html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-</head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
     <body>
         <h1>ファイルアップロードテスト</h1>
+        {{-- ここからがファイルドラックアンドドロップ式の部分 --}}
         <div id="droppable" style="border: gray solid 1em; padding: 2em;">
         ファイルをドロップしてください。
         </div>
         <button id="hugefile" type="submit" class="btn btn-primary">送信</button>
-
         <br>
         <br>
+        {{-- ここからがファイル選択式の部分 --}}
         <input type="file" name="upfile" id="upfile" accept="image/*">
 	    <button type="button" name="button" id="button">送信する</button>
 
         <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
         <script>
-            let upload_file;
+            let upload_file; //グローバル変数
             $(function() {
                 var droppable = $("#droppable");
 
@@ -26,12 +27,14 @@
                     return false;
                 }
 
+                // イベントをキャンセルするハンドラ
                 var cancelEvent = function(event) {
                     event.preventDefault();
                     event.stopPropagation();
                     return false;
                 }
 
+                // dragenter, dragover イベントのデフォルト処理をキャンセルします
                 droppable.bind("dragenter", cancelEvent);
                 droppable.bind("dragover", cancelEvent);
 
@@ -39,11 +42,16 @@
                 var handleDroppedFile = function(event) {
                     // ファイルは複数ドロップされる可能性がありますが, ここでは 1 つ目のファイルを扱います.
                     var file = event.originalEvent.dataTransfer.files[0];
+                    // event.target.result に読み込んだファイルの内容が入っています.
+                    // ドラッグ＆ドロップでファイルアップロードする場合は result の内容を Ajax で送信
                     $("#droppable").text("[" + file.name + "]");
                     upload_file = file
+
+                    // デフォルトの処理をキャンセルします.
                     cancelEvent(event);
                     return false;
                 }
+                // ドロップ時のイベントハンドラを設定します.
                 droppable.bind("drop", handleDroppedFile);
             });
 
@@ -53,17 +61,18 @@
 
             // ボタンが押されたときの処理
             $("#hugefile").on('click', function () {
+                // ファイル選択がない場合
                 if (upload_file == null) {
                     alert('ファイルがない')
                     return false
                 }
-
+                // GETリクエストで署名済みURLと一意のファイル名を取得する
                 $.ajax({
                     url: "{{route('s3.getPresignedUrl')}}",
                     data: {"filename":upload_file.name},
                     type: 'GET',
                     dataType: 'json'
-                }).done(data => sendFileCore(data, upload_file))
+                }).done(data => sendFileCore(data, upload_file)) // アップロード
                 return false
             })
 
@@ -78,6 +87,7 @@
                     processData: false
                 })
                 .done(function(json_data) {
+                    $("#droppable").text("");
                     alert("S3保存完了");
                 })
                 .fail(function() {
@@ -89,41 +99,25 @@
         {{-- インプットでファイル選択タイプのスクリプト --}}
         <script>
             $(function(){
-                let upload_file;
+                let upload_file; //グローバル変数
 
+                // ファイルを選択すると発火しfileを読み込む
                 $("#upfile").change(function(){
                     console.log("upload file");
                     if (this.files.length > 0) {
                         upload_file = this.files[0];
                     }
                 });
-
+                // ボタンが押されたときの処理
                 $("#button").click(function(){
                     var filetype = upload_file["type"];
-
+                    // GETリクエストで署名済みURLと一意のファイル名を取得する
                     $.ajax({
                         url: "{{route('s3.getPresignedUrl')}}",
                         data: {"filename":upload_file.name},
                         type: 'GET',
                         dataType: 'json'
-                    }).done(data => sendFileCore(data, upload_file))
-
-                    // $.ajax({
-                    //     type: "PUT",
-                    //     url: pre_signed_url,
-                    //     data: file,
-                    //     headers:{
-                    //         "Content-Type":filetype
-                    //     },
-                    //     processData: false,
-                    //     contentType: false
-                    // })
-                    // .done(function(json_data) {
-                    //     console.log("success");
-                    // })
-                    // .fail(function() {
-                    //     alert("error");
-                    // });
+                    }).done(data => sendFileCore(data, upload_file)) // アップロード
                 });
             });
 
